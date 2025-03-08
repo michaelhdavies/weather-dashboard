4d95ad2339a7ef00dc403fdfaf85e6d7e9f67294
Michael Henry Davies <michael_methodtheatre.com>
2025-03-08 15:17:56 -0700

    first pass of changes - worked on a few files but nothing works yet.

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -5,6 +5,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// TODO: Define route to serve index.html
+// DONE?: Define route to serve index.html
+router.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+});
 
 export default router;
